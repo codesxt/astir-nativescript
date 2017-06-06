3f1ed363163975c498bddd6c86a92e16c9cf9222
Bruno Alexis Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2017-06-06 12:22:18 -0400

    Add 'nativescript-directions' plugin to show event location.

--- app/pages/event-details/event-details.component.ts
+++ app/pages/event-details/event-details.component.ts
@@ -3,6 +3,7 @@ import { ModalDialogService } from "nativescript-angular/directives/dialogs";
 import { RouterExtensions } from 'nativescript-angular/router';
 import { ActivatedRoute } from "@angular/router";
 import { Event } from "../../shared/event/event";
+import { Directions } from "nativescript-directions";
 
 import { CalendarModalComponent } from "../calendar-modal/calendar.modal";
 
@@ -15,13 +16,14 @@ import { CalendarModalComponent } from "../calendar-modal/calendar.modal";
 export class EventDetailsComponent implements OnInit {
     text: string = 'Detalles del Evento';
     event: Event = null;
-
+    directions: any;
     constructor(
       private routerExtensions: RouterExtensions,
       private route: ActivatedRoute,
       private modal: ModalDialogService,
       private vcRef: ViewContainerRef
     ) {
+      this.directions = new Directions();
     }
 
     ngOnInit(){
@@ -44,6 +46,42 @@ export class EventDetailsComponent implements OnInit {
       };
       this.modal.showModal(CalendarModalComponent, options).then(res => {
 
+      });
+    }
+
+    openInMap(){
+      this.directions.available().then((avail) => {
+        if(!avail){
+          alert({
+            title: "Error",
+            message: "No hay una aplicación de mapas disponible.",
+            okButtonText: "Entendido"
+          });
+        }else{
+          let destination = null;
+          if(!this.event.where.location){
+            destination = {
+              address: this.event.where.address
+            }
+          }else{
+            destination = {
+              lat: this.event.where.location.coordinates[1],
+              lng: this.event.where.location.coordinates[0]
+            }
+          }
+          this.directions.navigate({
+            to: destination
+          }).then(() => {
+              console.log("Maps app launched.");
+          }, (error) => {
+              console.log(error);
+              alert({
+                title: "Error",
+                message: "Ocurrió un error al abrir la aplicación de mapas.",
+                okButtonText: "Entendido"
+              })
+          });
+        }
       });
     }
 }
